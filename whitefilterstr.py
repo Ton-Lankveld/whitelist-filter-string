87c5eb65d5c58d2bc825b7607fec4e811804c257
Ton van Lankveld <Ton-Lankveld@users.noreply.github.com>
2018-01-16 19:21:15 +0100

    Corrected typo's

--- whitefilterstr.py
+++ whitefilterstr.py
@@ -1,12 +1,12 @@
 def whiteListCharFilter(inStr, whiteListStr):
-    """ Sanatize a string with a list of allowed (white) characters
+    """ Sanitize a string with a list of allowed (white) characters
 
-    Input: inStr {string} String to be sanatized.
+    Input: inStr {string} String to be sanitized.
     Input: whiteListStr {string} String with allowed characters.
-    Output: outStr {string} Sanatized string
+    Output: outStr {string} Sanitized string
     """
     outStr = ""
-    if (isinstance(inStr, str) or isinstance(whiteListStrinStr, str)) == False:
+    if (isinstance(inStr, str) or isinstance(whiteListStr, str)) == False:
         return outStr
     for characterStr in inStr:
         if characterStr in whiteListStr:
